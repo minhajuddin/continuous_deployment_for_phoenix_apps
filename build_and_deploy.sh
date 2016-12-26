5ee3bb71f821a38d5d6c4f99d79e1dfaea73b7e4
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-12-26 22:56:58 +0530

    rebuild book

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+log(){
+  echo "> $*"
+}
+
 # init commands
 #mkdir -p _book
 #cd _book
@@ -10,15 +14,19 @@ set -e
 #git checkout -b gh-pages
 
 # build all versions
+log "building book"
 gitbook build
 #gitbook pdf _books/continuous-deployment-for-phoenix-apps.pdf
 #gitbook mobi _books/continuous-deployment-for-phoenix-apps.mobi
 #gitbook epub _books/continuous-deployment-for-phoenix-apps.epub
 
+log "committing code"
 cd _book
 #git init
 #git checkout -b gh-pages
 echo 'continuous-deployment-for-phoenix-apps.rawcode.in' > CNAME
 git add .
 git commit -am 'rebuild book'
+
+log "pushing to github"
 git push origin gh-pages
